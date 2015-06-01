f75407a71328eba18209fa8333f6dcb433e1e545
Ben Lesh <blesh@netflix.com>
2015-06-01 12:01:32 -0700

    update rxjs version

--- blueprints/ember-cli-rx/index.js
+++ blueprints/ember-cli-rx/index.js
@@ -2,7 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('rxjs', '~2.3.24');
+    return this.addBowerPackageToProject('rxjs', '~2.5.2');
   }
 };
 
